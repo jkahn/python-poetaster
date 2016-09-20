9d114f81c5da8c6e4c1e21b9f428a5d03482602f
Jeremy G. Kahn <jeremy@trochee.net>
2016-09-19 22:17:52 -0700

    Added formatting arguments and tests for haiku.

--- src/poetaster/haiku.py
+++ src/poetaster/haiku.py
@@ -2,6 +2,7 @@
 """Detects (and formats?) haiku in input strings."""
 import six
 from poetaster.lattice import IslexOrthoLattice
+from poetaster.lattice import SegmentationError
 
 
 class NotHaiku(ValueError):
@@ -12,6 +13,19 @@ class NotHaiku(ValueError):
 
 class Haiku(object):
     """Core components of a haiku."""
+    @classmethod
+    def from_transduction(cls, t, s):
+        """Returns haiku (or None) given transduction and string."""
+        lines = []
+        for b, e in ((0, 5), (5, 12), (12, 17)):
+            try:
+                lines.append(t.syllable_range(b, e))
+            except SegmentationError:
+                return None  # No word break at appropriate syll boundary
+        if t.syllable_count != 17:
+            return None
+        return cls(s, t, _lines=lines)
+
     @classmethod
     def from_string(cls, s):
         """Returns list of all Haiku transductions possible.  If no
@@ -28,16 +42,33 @@ class Haiku(object):
         if not len(transductions):
             raise NotHaiku("Could not tile string with ortho representations.")
 
-        haikus = [cls(s, t) for t in transductions if t.syllable_count == 17]
+        haikus = []
+        for t in transductions:
+            h = cls.from_transduction(t, s)
+            if h:
+                haikus.append(h)
 
         if not haikus:
             raise NotHaiku("No syllabification has 17 syllables (found %s)"
                            % [t.syllable_count for t in transductions])
 
-        # Filter for 5/7/5 pattern?
-
         return haikus
 
-    def __init__(self, raw, transduction):
+    def __init__(self, raw, transduction, _lines=None):
         self.raw = raw
         self.transduction = transduction
+        self._lines = _lines
+
+    @property
+    def lines(self):
+        if not self._lines:
+            # extract lines
+            self._lines = [self.transduction.syllable_range(b, e)
+                           for b, e in ((0, 5), (5, 12), (12, 17))]
+        return self._lines
+
+    @property
+    def formatted(self):
+        line_tokens = (l.retokenization for l in self.lines)
+        lines = (' '.join(ws) for ws in line_tokens)
+        return "\n".join(s.capitalize() for s in lines)
--- tests/test_haiku.py
+++ tests/test_haiku.py
@@ -3,8 +3,11 @@
 from __future__ import print_function
 import pytest
 
+from six import string_types
+
 from poetaster.haiku import Haiku
 from poetaster.haiku import NotHaiku
+from poetaster.lattice import Transduction
 
 def assert_non_haiku(inp, message):
     """Utility function for non-haikus."""
@@ -35,9 +38,35 @@ def test_not_haiku():
                      "but that will not stop me from trying",  # nine
                      "No syllabification has 17 syllables")
 
+TEST_HAIKU = (
+    "The autumn leaves fall "  # five
+    "gently into my basket "  # seven
+    "how I knew them green."  # five
+)
+
+TARGET = """\
+The autumn leaves fall
+Gently into my basket
+How i knew them green"""
 
 def test_haikus():
-    assert_haiku("The autumn leaves fall "  # five
-                 "gently into my basket "  # seven
-                 "how I knew them green"  # five
+    haikus = assert_haiku(TEST_HAIKU)
+    assert len(haikus) == 1
+
+
+def test_format_haiku():
+    haiku = assert_haiku(TEST_HAIKU)[0]
+    assert len(haiku.lines) == 3
+    for l in haiku.lines:
+        assert isinstance(l, Transduction)
+
+    assert tuple(l.syllable_count for l in haiku.lines) == (5, 7, 5)
+
+    assert tuple(l.retokenization for l in haiku.lines) == (
+        ("the", "autumn", "leaves", "fall"),
+        ("gently", "into", "my", "basket"),
+        ("how", "i", "knew", "them", "green")
     )
+
+    assert isinstance(haiku.formatted, string_types)
+    assert haiku.formatted == TARGET
